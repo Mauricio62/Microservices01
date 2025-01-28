6f40d1af6a3e774aa882d05a151d8987bd9495ac
Mauricio62 <ericksandovalv08@gmail.com>
2025-01-28 17:51:17 -0500

    productService

---
+++ spring-init/api/src/main/java/pe/joedayz/microservices/api/core/recommendation/Recommendation.java
@@ -0,0 +1,4 @@
+package pe.joedayz.microservices.api.core.recommendation;
+
+public class Recommendation {
+}
---
+++ spring-init/api/src/main/java/pe/joedayz/microservices/api/core/review/Review.java
@@ -0,0 +1,4 @@
+package pe.joedayz.microservices.api.core.review;
+
+public class Review {
+}
